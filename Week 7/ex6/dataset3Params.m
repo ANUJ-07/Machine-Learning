ee51680c83a473346984f35a79e857088c0272f7
Anuj <anujpriyadarshi07@gmail.com>
2017-11-25 22:47:38 +0530

    Week 7 Spam part finished

--- Week 7/ex6/dataset3Params.m
+++ Week 7/ex6/dataset3Params.m
@@ -8,9 +8,23 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
+min_res = 99999999;
+ss = [0.01,0.03,0.1,0.3,1,3,10,30]';
+CC = [0.01,0.03,0.1,0.3,1,3,10,30]';
 C = 1;
 sigma = 0.3;
-
+for i = 1:8
+  for j = 1:8
+    model = svmTrain(X, y, CC(i), @(x1,x2) gaussianKernel(x1, x2, ss(j)));
+    predictions = svmPredict(model, Xval);
+    temp = mean(double(predictions ~= yval));
+    if min_res > temp
+      min_res = temp;
+      C = CC(i);
+      sigma = ss(j);
+    end
+  end
+end
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -19,15 +33,10 @@ sigma = 0.3;
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
+
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
-
-
-
-
 
 % =========================================================================
 
