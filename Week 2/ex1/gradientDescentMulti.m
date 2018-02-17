39d303cd931e661872401ce19a309c4d011f152b
Anuj <anujpriyadarshi07@gmail.com>
2018-02-17 11:37:14 +0530

    Week 8 9 late push

--- Week 2/ex1/gradientDescentMulti.m
+++ Week 2/ex1/gradientDescentMulti.m
@@ -16,18 +16,6 @@ for iter = 1:num_iters
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
-    %
-
-
-
-
-
-
-
-
-
-
-
     % ============================================================
 
     % Save the cost J in every iteration    
